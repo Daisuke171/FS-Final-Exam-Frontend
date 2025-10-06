47d7a1986490806c8d74cb2734d40ed3340f3af0
Nicron <nicron007@gmail.com>
2025-10-06 01:58:04 +0000

    feat(match): added all the match page

---
+++ app/games/rock-paper-scissors/[roomId]/match/game-component.tsx
@@ -0,0 +1,349 @@
+"use client";
+
+import Card from "@/components/game/rock-paper-scissors/cards/Card";
+import { Icon, IconifyIcon } from "@iconify/react";
+import FistIcon from "@iconify-icons/game-icons/fist";
+import HandIcon from "@iconify-icons/game-icons/hand";
+import ScissorsIcon from "@iconify-icons/game-icons/scissors";
+import { motion, AnimatePresence, Easing } from "motion/react";
+import CustomButton from "@/components/game/rock-paper-scissors/buttons/CustomButtonTwo";
+import PlayersInRoom from "@/components/game/rock-paper-scissors/general/PlayersInRoom";
+import NumbersAnimation from "@/components/game/rock-paper-scissors/general/NumbersAnimation";
+import { useGameSocket } from "@/hooks/rock-paper-scissors/useGameSocket";
+import { useParams } from "next/navigation";
+import CountdownCard from "@/components/game/rock-paper-scissors/cards/CountdownCard";
+import { useEffect } from "react";
+import { getSocket } from "@/app/socket";
+import RenderBattleAnimation from "@/components/game/rock-paper-scissors/general/RenderBattleAnimation";
+
+interface GameResult {
+  text: string;
+  icon: string;
+  styles: string;
+  points: {
+    number: number;
+    symbol: string;
+  };
+}
+
+interface ButtonProps {
+  title: string;
+  img: IconifyIcon;
+  move: string;
+}
+
+export default function GameComponent() {
+  const params = useParams();
+  const roomId = params.roomId || "";
+
+  const socket = getSocket();
+  const {
+    showBattleAnimation,
+    battleStage,
+    playedMovements,
+    playerHealth,
+    healthDamage,
+    winner,
+    gameWinner,
+    countDown,
+    confirmedPlayers,
+    confirmed,
+    clicked,
+    timeLeft,
+    playerId,
+    players,
+    handlePlayAgain,
+    handleConfirmMove,
+    disableCards,
+    handlePlayerMove,
+    state,
+    totalDuration,
+    previousHealth,
+  } = useGameSocket(roomId);
+
+  useEffect(() => {
+    socket.emit("requestGameState", { roomId });
+    return () => {
+      socket.off("requestGameState");
+    };
+  }, []);
+
+  const Buttons: ButtonProps[] = [
+    {
+      title: "Piedra",
+      img: FistIcon,
+      move: "piedra",
+    },
+    {
+      title: "Papel",
+      img: HandIcon,
+      move: "papel",
+    },
+    {
+      title: "Tijera",
+      img: ScissorsIcon,
+      move: "tijera",
+    },
+  ];
+
+  const getHealthColor = (health: number) => {
+    if (health > 60) return "bg-emerald-500";
+    if (health > 30) return "bg-yellow-600";
+    if (health > 10) return "bg-orange-500";
+    return "bg-rose-500";
+  };
+
+  const gameResult: GameResult[] = [
+    gameWinner === playerId
+      ? {
+          text: "¡Has ganado!",
+          icon: "game-icons:laurel-crown",
+          styles: "text-emerald-700",
+          points: {
+            number: 20,
+            symbol: "+",
+          },
+        }
+      : gameWinner === "tie"
+      ? {
+          text: "Empate",
+          icon: "game-icons:crossed-swords",
+          styles: "text-slate-500",
+          points: {
+            number: 5,
+            symbol: "+",
+          },
+        }
+      : {
+          text: "Has perdido",
+          icon: "game-icons:dead-head",
+          styles: "text-rose-800",
+          points: {
+            number: 20,
+            symbol: "-",
+          },
+        },
+  ].filter(Boolean);
+
+  if (countDown !== null && countDown > 0) {
+    return <CountdownCard countDown={countDown} />;
+  }
+
+  return (
+    <AnimatePresence mode="popLayout">
+      {state === "FinishedState" ? (
+        <motion.div
+          key="game-over"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.5 }}
+          className="flex flex-col h-full items-center p-10 bg-slate-300 m-auto w-100 rounded-xl border-2 border-slate-600"
+        >
+          <p className="text-2xl mt-2 text-slate-700"></p>
+          {gameResult.map((r, idx) => (
+            <div
+              className={
+                "flex flex-col items-center gap-2 mb-5 px-4 text-slate-800"
+              }
+              key={idx}
+            >
+              <Icon
+                icon={r.icon}
+                width={100}
+              />
+              <p className="text-4xl font-bold">{r.text}</p>
+              <motion.p
+                initial={{ y: 50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1, transition: { delay: 0.5 } }}
+                className={`text-2xl font-bold ${r.styles}`}
+              >
+                {r.points.symbol}
+                <NumbersAnimation
+                  value={r.points.number}
+                  delay={1}
+                />{" "}
+                puntos
+              </motion.p>
+            </div>
+          ))}
+          <div className="mb-6">
+            <PlayersInRoom
+              players={players}
+              confirmedPlayers={confirmedPlayers}
+              playerId={playerId}
+              label
+            />
+          </div>
+          <CustomButton
+            text="Volver a jugar"
+            icon="game-icons:rapidshare-arrow"
+            onClick={handlePlayAgain}
+          />
+        </motion.div>
+      ) : state === "PlayingState" || state === "RevealingState" ? (
+        <motion.div
+          key="game"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.5 }}
+          className="flex flex-col items-center bg-slate-300 w-150 m-auto rounded-xl border-2 border-slate-900"
+        >
+          <div className="rounded-t-xl h-70 w-full flex flex-col">
+            <div className="relative w-full">
+              <div className="bg-slate-800 h-60 w-full rounded-t-xl"></div>
+              <div className="bg-slate-900 h-20 w-full"></div>
+              <div className="absolute top-5 flex justify-between w-full px-5">
+                <div className="space-y-6 flex w-full justify-between">
+                  {Object.keys(playerHealth)
+                    .sort((a, b) =>
+                      a === playerId ? -1 : b === playerId ? 1 : 0
+                    )
+                    .map((id) => {
+                      const oldHealth =
+                        previousHealth[id] || playerHealth[id] || 100;
+                      const newHealth = playerHealth[id] || 100;
+                      const isPlayer = id === playerId;
+
+                      return (
+                        <div
+                          key={id}
+                          className=""
+                        >
+                          <div
+                            key={id}
+                            className="space-y-2 w-60"
+                          >
+                            <div className="flex justify-between items-center px-1">
+                              <span
+                                className={` ${
+                                  isPlayer ? "text-indigo-500" : "text-rose-500"
+                                }`}
+                              >
+                                {isPlayer ? "Tu Vida" : "Vida del Rival"}
+                              </span>
+                              <div className="flex items-center space-x-2">
+                                <span className="text-sm line-through text-slate-500">
+                                  {oldHealth} HP
+                                </span>
+                                <span className="text-sm font-bold text-rose-400">
+                                  <NumbersAnimation value={newHealth} /> HP
+                                </span>
+                              </div>
+                            </div>
+                            <div
+                              className={`relative w-full h-6 mb-2 bg-slate-900 rounded-full overflow-hidden border-2 border-indigo-200 ${
+                                newHealth > 20 && newHealth <= 40
+                                  ? "animate-[vibrate-2_linear_0.4s_infinite]"
+                                  : newHealth <= 20
+                                  ? "animate-[vibrate-1_linear_0.3s_infinite]"
+                                  : ""
+                              }`}
+                            >
+                              <motion.div
+                                initial={{ width: `${oldHealth}%` }}
+                                animate={{ width: `${newHealth}%` }}
+                                transition={{
+                                  duration: 1.5,
+                                  ease: "easeOut",
+                                  delay: 0.7,
+                                }}
+                                className={`absolute h-8 ${getHealthColor(
+                                  newHealth
+                                )} opacity-50`}
+                              />
+                              <motion.div
+                                initial={{ width: `${oldHealth}%` }}
+                                animate={{ width: `${newHealth}%` }}
+                                transition={{ duration: 1.5, ease: "easeOut" }}
+                                className={`h-8 ${getHealthColor(
+                                  newHealth
+                                )} relative z-10`}
+                              />
+                            </div>
+                          </div>
+
+                          <AnimatePresence mode="popLayout">
+                            {battleStage === "damage" && (
+                              <motion.div
+                                initial={{ opacity: 0, y: -10, scale: 0.5 }}
+                                animate={{ opacity: 1, y: 0, scale: 1 }}
+                                exit={{ opacity: 0, y: -10, scale: 0.5 }}
+                                transition={{ delay: 0.5, duration: 0.2 }}
+                                className="text-center "
+                              >
+                                {winner !== id && (
+                                  <span
+                                    className={`inline-block px-3 py-1 rounded-full text-sm font-bold bg-rose-300 text-rose-800`}
+                                  >
+                                    -{healthDamage[id] || 0} HP
+                                  </span>
+                                )}
+                              </motion.div>
+                            )}
+                          </AnimatePresence>
+                        </div>
+                      );
+                    })}
+                </div>
+                <div className="flex items-center">
+                  {showBattleAnimation && (
+                    <RenderBattleAnimation
+                      showBattleAnimation={showBattleAnimation}
+                      playedMovements={playedMovements}
+                      playerId={playerId}
+                      battleStage={battleStage}
+                      winner={winner}
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="p-8">
+            <AnimatePresence mode="wait">
+              {timeLeft !== null && timeLeft >= 0 && (
+                <motion.div
+                  initial={{ height: 0, opacity: 0 }}
+                  animate={{ height: "auto", opacity: 1 }}
+                  exit={{ height: 0, opacity: 0 }}
+                  className="w-full flex flex-col items-center mt-5"
+                >
+                  <p className="text-2xl mt-2 text-slate-800">
+                    Tiempo restante: {timeLeft}s
+                  </p>
+                  <div className="w-full h-2 rounded-full mt-2 bg-slate-500 relative overflow-hidden">
+                    <motion.div
+                      animate={{ scaleX: timeLeft / totalDuration }}
+                      transition={{ duration: 0.1, ease: "linear" }}
+                      className="absolute top-0 left-0 h-2 bg-slate-900 rounded-full"
+                      style={{ width: "100%", transformOrigin: "left" }}
+                    ></motion.div>
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+            <div className="flex gap-5 w-full items-center justify-center my-10">
+              {Buttons.map((b) => (
+                <Card
+                  key={b.title}
+                  title={b.title}
+                  img={b.img}
+                  isClicked={clicked === b.move}
+                  onClick={() => handlePlayerMove(b.move)}
+                  disableCards={disableCards}
+                />
+              ))}
+            </div>
+            <CustomButton
+              text="Confirmar"
+              onClick={handleConfirmMove}
+              clicked={clicked}
+              confirmed={confirmed}
+              icon="game-icons:confirmed"
+            />
+          </div>
+        </motion.div>
+      ) : null}
+    </AnimatePresence>
+  );
+}
