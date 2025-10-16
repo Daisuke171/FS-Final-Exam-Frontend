eaacca4625a56eea06033b643425900d115d2e98
Nicron <nicron007@gmail.com>
2025-10-16 02:49:35 +0000

    refactor(profile): the hardcoded data has been redone to include the backend data

--- app/profile/match-history.tsx
+++ app/profile/match-history.tsx
@@ -1,156 +1,177 @@
 "use client";
 
-import MatchHistoryComponent, {
-  MatchHistoryComponentProps,
-} from "@/components/ui/general/profile/MatchHistoryComponent";
-import CustomSelect from "@/components/ui/inputs/CustomSelect";
+import MatchHistoryComponent from "@/components/ui/general/profile/MatchHistoryComponent";
 import { Icon } from "@iconify/react";
 import { useState } from "react";
 import { motion } from "motion/react";
+import { GameHistory } from "@/types/game.types";
+import { GET_USER_GAMES } from "@/graphql/queries/game.queries";
+import { useQuery } from "@apollo/client/react";
+import MatchHistoryLayout from "@/components/layout/profile/match-history-layout";
+import MatchSkeleton from "@/components/ui/skeletons/profile/MatchSkeleton";
 
 type FilterType = "all" | "win" | "lose" | "draw";
 
-export const matches: MatchHistoryComponentProps[] = [
-  {
-    logo: "/games/rock-paper-scissors/rps-logo-white.png",
-    result: "win",
-    points: 150,
-    date: "12 Oct 2025",
-    duration: "2m 15s",
-    game: "Piedra, papel o tijera",
-  },
-  {
-    logo: "/games/rock-paper-scissors/rps-logo-white.png",
-    result: "lose",
-    points: 75,
-    date: "10 Oct 2025",
-    duration: "1m 42s",
-    game: "Adivina el número",
-  },
-  {
-    logo: "/games/rock-paper-scissors/rps-logo-white.png",
-    result: "draw",
-    points: 20,
-    date: "9 Oct 2025",
-    duration: "3m 05s",
-    game: "Trivia Express",
-  },
-  {
-    logo: "/games/rock-paper-scissors/rps-logo-white.png",
-    result: "win",
-    points: 200,
-    date: "7 Oct 2025",
-    duration: "1m 30s",
-    game: "Piedra, papel o tijera",
-  },
-  {
-    logo: "/games/rock-paper-scissors/rps-logo-white.png",
-    result: "lose",
-    points: 50,
-    date: "6 Oct 2025",
-    duration: "4m 10s",
-    game: "Memory Challenge",
-  },
-  {
-    logo: "/games/rock-paper-scissors/rps-logo-white.png",
-    result: "lose",
-    points: 75,
-    date: "10 Oct 2025",
-    duration: "1m 42s",
-    game: "Adivina el número",
-  },
-  {
-    logo: "/games/rock-paper-scissors/rps-logo-white.png",
-    result: "draw",
-    points: 20,
-    date: "9 Oct 2025",
-    duration: "3m 05s",
-    game: "Trivia Express",
-  },
-  {
-    logo: "/games/rock-paper-scissors/rps-logo-white.png",
-    result: "win",
-    points: 200,
-    date: "7 Oct 2025",
-    duration: "1m 30s",
-    game: "Piedra, papel o tijera",
-  },
-  {
-    logo: "/games/rock-paper-scissors/rps-logo-white.png",
-    result: "lose",
-    points: 50,
-    date: "6 Oct 2025",
-    duration: "4m 10s",
-    game: "Memory Challenge",
-  },
-];
+// export const matches: MatchHistoryComponentProps[] = [
+//   {
+//     logo: "/games/rock-paper-scissors/rps-logo-white.png",
+//     result: "win",
+//     points: 150,
+//     date: "12 Oct 2025",
+//     duration: "2m 15s",
+//     game: "Piedra, papel o tijera",
+//   },
+//   {
+//     logo: "/games/rock-paper-scissors/rps-logo-white.png",
+//     result: "lose",
+//     points: 75,
+//     date: "10 Oct 2025",
+//     duration: "1m 42s",
+//     game: "Adivina el número",
+//   },
+//   {
+//     logo: "/games/rock-paper-scissors/rps-logo-white.png",
+//     result: "draw",
+//     points: 20,
+//     date: "9 Oct 2025",
+//     duration: "3m 05s",
+//     game: "Trivia Express",
+//   },
+//   {
+//     logo: "/games/rock-paper-scissors/rps-logo-white.png",
+//     result: "win",
+//     points: 200,
+//     date: "7 Oct 2025",
+//     duration: "1m 30s",
+//     game: "Piedra, papel o tijera",
+//   },
+//   {
+//     logo: "/games/rock-paper-scissors/rps-logo-white.png",
+//     result: "lose",
+//     points: 50,
+//     date: "6 Oct 2025",
+//     duration: "4m 10s",
+//     game: "Memory Challenge",
+//   },
+//   {
+//     logo: "/games/rock-paper-scissors/rps-logo-white.png",
+//     result: "lose",
+//     points: 75,
+//     date: "10 Oct 2025",
+//     duration: "1m 42s",
+//     game: "Adivina el número",
+//   },
+//   {
+//     logo: "/games/rock-paper-scissors/rps-logo-white.png",
+//     result: "draw",
+//     points: 20,
+//     date: "9 Oct 2025",
+//     duration: "3m 05s",
+//     game: "Trivia Express",
+//   },
+//   {
+//     logo: "/games/rock-paper-scissors/rps-logo-white.png",
+//     result: "win",
+//     points: 200,
+//     date: "7 Oct 2025",
+//     duration: "1m 30s",
+//     game: "Piedra, papel o tijera",
+//   },
+//   {
+//     logo: "/games/rock-paper-scissors/rps-logo-white.png",
+//     result: "lose",
+//     points: 50,
+//     date: "6 Oct 2025",
+//     duration: "4m 10s",
+//     game: "Memory Challenge",
+//   },
+// ];
 
-export default function MatchHistory() {
+export default function MatchHistory({ userId }: { userId: string }) {
   const [filter, setFilter] = useState<FilterType>("all");
+  const { data, loading, error } = useQuery<{ userGames: GameHistory[] }>(
+    GET_USER_GAMES,
+    {
+      variables: { userId },
+    }
+  );
+
+  const matches = data?.userGames || [];
 
   const handleFilterGames = (value: FilterType) => {
     setFilter(value);
   };
 
+  const games = 8;
+
   const filteredMatches =
     filter === "all"
       ? matches
-      : matches.filter((match) => match.result === filter);
-  return (
-    <section className="bg-white/7 p-4 py-6 md:p-6 lg:p-8 w-[52%] rounded-2xl flex-1 flex flex-col ">
-      <div className="flex items-center gap-3 md:gap-0 flex-col md:flex-row justify-between">
-        <h2 className="text-xl lg:text-2xl font-medium text-font flex  items-center gap-2 ">
+      : matches.filter((match) => match.state === filter);
+
+  if (loading)
+    return (
+      <MatchHistoryLayout
+        action={(value) => handleFilterGames(value as FilterType)}
+      >
+        {Array.from({ length: games }).map((_, index) => (
+          <MatchSkeleton key={index} />
+        ))}
+      </MatchHistoryLayout>
+    );
+  if (error)
+    return (
+      <MatchHistoryLayout
+        action={(value) => handleFilterGames(value as FilterType)}
+      >
+        <div className="flex flex-col flex-1 h-full items-center justify-center">
           <Icon
-            icon="solar:book-bold"
-            className="text-2xl md:text-3xl"
+            icon="fluent-emoji-high-contrast:sad-but-relieved-face"
+            className="text-font text-6xl lg:text-8xl"
           />
-          Historial <span className="text-subtitle">(últimas 10 partidas)</span>
-        </h2>
-        <CustomSelect
-          options={[
-            { value: "all", label: "Todos" },
-            { value: "win", label: "Victorias" },
-            { value: "lose", label: "Derrotas" },
-            { value: "draw", label: "Empates" },
-          ]}
-          onChange={(value) => handleFilterGames(value as FilterType)}
-          placeholder="Todos"
-          defaultValue="all"
-        />
-      </div>
-      <div className="flex flex-col mt-5 md:mt-10 gap-3 flex-grow pr-1 lg:pr-2 w-full min-h-[200px] max-h-150 md:max-h-181 [@media(min-width:913px)_and_(max-width:1024px)]:max-h-111 lg:max-h-250 xl:max-h-186 overflow-y-auto custom-scrollbar">
-        {matches.length == 0 ? (
-          <div className="flex flex-col flex-1 h-full items-center justify-center">
-            <Icon
-              icon="solar:gamepad-no-charge-broken"
-              width="100"
-              className="text-font"
+          <h2 className="text-xl font-medium text-font mt-5 text-center">
+            Se ha producido un error al tratar de obtener tu Historial
+          </h2>
+          <p className="text-subtitle">{error.message}</p>
+        </div>
+      </MatchHistoryLayout>
+    );
+
+  return (
+    <MatchHistoryLayout
+      action={(value) => handleFilterGames(value as FilterType)}
+    >
+      {matches.length == 0 ? (
+        <div className="flex flex-col flex-1 h-full items-center justify-center">
+          <Icon
+            icon="solar:gamepad-no-charge-broken"
+            width="100"
+            className="text-font"
+          />
+          <h2 className="text-xl font-medium text-font mt-5">
+            No has jugado ninguna partida aún
+          </h2>
+        </div>
+      ) : (
+        filteredMatches.map((match, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: (index + 2) * 0.1 }}
+            className="w-full"
+          >
+            <MatchHistoryComponent
+              logo={match.game.gameLogo}
+              result={match.state}
+              points={match.score}
+              date={new Date(match.createdAt).toLocaleDateString()}
+              duration={match.duration}
+              game={match.game.name}
             />
-            <h2 className="text-xl font-medium text-font mt-5">
-              No has jugado ninguna partida aún
-            </h2>
-          </div>
-        ) : (
-          filteredMatches.map((match, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0, x: -20 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ delay: (index + 2) * 0.1 }}
-              className="w-full"
-            >
-              <MatchHistoryComponent
-                logo={match.logo}
-                result={match.result}
-                points={match.points}
-                date={match.date}
-                duration={match.duration}
-                game={match.game}
-              />
-            </motion.div>
-          ))
-        )}
-      </div>
-    </section>
+          </motion.div>
+        ))
+      )}
+    </MatchHistoryLayout>
   );
 }
--- app/profile/profile-header.tsx
+++ app/profile/profile-header.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import StatCard, {
-  StatCardProps,
-} from "@/components/ui/cards/profile/StatCard";
-import ChipOne from "@/components/ui/chips/profile/ChipOne";
+import { StatCardProps } from "@/components/ui/cards/profile/StatCard";
 import ChangeAvatar from "@/components/ui/modals/profile/ChangeAvatar";
-import { Icon } from "@iconify/react";
 import { useState } from "react";
 import { AnimatePresence } from "motion/react";
+import ProfileHeaderStats from "@/components/ui/general/profile/ProfileHeaderStats";
+import LevelBar from "@/components/ui/general/profile/LevelBar";
+import ProfileHeaderInfo from "@/components/ui/general/profile/ProfileHeaderInfo";
+import { useQuery } from "@apollo/client/react";
+import { User } from "@/types/game.types";
+import { GET_ME } from "@/graphql/queries/user.queries";
 
 const cards: StatCardProps[] = [
   {
@@ -36,9 +38,14 @@ const cards: StatCardProps[] = [
   },
 ];
 
-export default function ProfileHeader() {
+export default function ProfileHeader({ userId }: { userId: string }) {
+  const { data, loading, error } = useQuery<{ user: User }>(GET_ME, {
+    variables: {
+      userId,
+    },
+  });
+  const user = data?.user;
   const [open, setOpen] = useState(false);
-  const [show, setShow] = useState(false);
 
   const handleModalOpen = () => {
     setOpen(!open);
@@ -47,121 +54,24 @@ export default function ProfileHeader() {
     <>
       <section className="bg-white/7 backdrop-blur-md max-w-350 p-4 py-6 lg:p-8 xl:px-10 xl:py-8 rounded-b-2xl w-[95%]">
         <div className="md:items-center flex flex-col  md:flex-row gap-y-5 lg:flex lg:flex-row justify-center lg:justify-between w-full">
-          <article className="flex justify-between flex-shrink-0 md:pr-8 xl:pr-0 items-center gap-8">
-            <div className="flex items-center gap-5">
-              <div className="h-26 w-26 xl:h-30 xl:w-30  cursor-pointer group flex items-center relative justify-center overflow-hidden rounded-full bg-background border border-dark-gray">
-                <Icon
-                  icon="mdi:user"
-                  className="text-subtitle text-[75px] xl:text-[85px]"
-                />
-                <div
-                  onClick={handleModalOpen}
-                  className="w-full hidden lg:block h-full absolute backdrop-blur-md transition-all duration-300 opacity-0 group-hover:opacity-100 bg-black/80 rounded-full"
-                ></div>
-                <Icon
-                  onClick={handleModalOpen}
-                  icon="mdi:account-cog"
-                  width="50"
-                  className="absolute hidden lg:block transition-all duration-300 text-font transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0"
-                />
-              </div>
-              <div>
-                <Icon
-                  onClick={handleModalOpen}
-                  icon="mdi:account-cog"
-                  width="30"
-                  className="lg:hidden flex place-self-start mb-1"
-                />
-                <div className="flex items-center gap-5">
-                  <h2 className="text-lg text-font font-medium flex items-center gap-1">
-                    Usuario123
-                    <Icon
-                      icon="material-symbols:verified"
-                      width="22"
-                      className="text-light-blue"
-                    />
-                  </h2>
-                  {/* Futura función para cambiar el nickname */}
-
-                  {/* <Icon
-                    icon="material-symbols:edit-rounded"
-                    width="22"
-                    className="hover:text-light-blue transition-all cursor-pointer"
-                  /> */}
-                </div>
-                <p className="text-subtitle">usuario@usuario.com</p>
-                <div className="[@media(min-width:458px)_and_(max-width:1024px)]:hidden flex items-center gap-2 lg:flex">
-                  <ChipOne
-                    value="2423"
-                    icon="ix:trophy-filled"
-                    color="secondary"
-                  />
-                  <ChipOne
-                    value="1443"
-                    icon="ix:coins-filled"
-                    color="primary"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="hidden flex-col [@media(min-width:458px)_and_(max-width:1024px)]:flex items-center lg:hidden">
-              <ChipOne
-                value="2423"
-                icon="ix:trophy-filled"
-                color="secondary"
-              />
-              <ChipOne
-                value="1443"
-                icon="ix:coins-filled"
-                color="primary"
-              />
-            </div>
-          </article>
+          <ProfileHeaderInfo
+            user={user}
+            error={error}
+            loading={loading}
+            action={handleModalOpen}
+          />
           <div className="w-[90%] h-0.5 bg-white/10 mx-auto md:hidden rounded-full"></div>
-          <article className="w-full flex-grow max-w-105 px-3 place-self-center xl:max-w-90 flex flex-col justify-center gap-2 md:border-l border-dark-gray h-23 md:pl-8 xl:pl-5">
-            <div className="flex justify-between items-center">
-              <p className="text-font text-base font-medium">Level 28</p>
-              <p className="text-font text-sm font-medium">
-                1210 <span className="text-subtitle">/ 2500</span>
-              </p>
-            </div>
-            <div className="relative w-full h-3 overflow-hidden rounded-2xl bg-background">
-              <div className="h-3 absolute w-1/2 bg-gradient-to-r from-shadow-blue to-medium-blue rounded-2xl"></div>
-            </div>
-            <div className="flex items-center mt-1 gap-1">
-              <div className="h-7 w-7 flex items-center justify-center rounded-full border border-medium-blue">
-                <p className="font-medium text-font">Ni</p>
-              </div>
-              <p className="text-font text-sm font-medium">Níquel</p>
-            </div>
-          </article>
-          <article className="place-self-center hidden w-full lg:w-auto lg:flex lg:place-self-auto">
-            <div className="text-center flex items-center justify-center">
-              {cards.map((card) => (
-                <StatCard
-                  key={card.title}
-                  icon={card.icon}
-                  title={card.title}
-                  number={card.number}
-                  color={card.color}
-                />
-              ))}
-            </div>
-          </article>
+          <LevelBar
+            user={user}
+            error={error}
+            loading={loading}
+          />
+          <ProfileHeaderStats userId={userId} />
         </div>
-        <article className="place-self-center w-full mt-5 lg:hidden">
-          <div className="text-center flex items-center justify-center">
-            {cards.map((card) => (
-              <StatCard
-                key={card.title}
-                icon={card.icon}
-                title={card.title}
-                number={card.number}
-                color={card.color}
-              />
-            ))}
-          </div>
-        </article>
+        <ProfileHeaderStats
+          userId={userId}
+          responsive
+        />
       </section>
       <AnimatePresence>
         {open && <ChangeAvatar closeModal={handleModalOpen} />}
