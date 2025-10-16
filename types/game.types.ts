3d471e8a609fd9d737ec3a58cd3b5a440a394509
Nicron <nicron007@gmail.com>
2025-10-16 02:46:58 +0000

    feat: created a file for the types of the backend data

---
+++ types/game.types.ts
@@ -0,0 +1,65 @@
+export interface UserStats {
+  winRate: number;
+  totalTime: string;
+  highScore: number;
+  bestStreak: number;
+  avgPerDay: number;
+  totalGames: number;
+  totalWins: number;
+  totalLosses: number;
+  totalDraws: number;
+  averageScore: number;
+}
+
+export interface LeaderboardEntry {
+  rank: number;
+  userId: string;
+  nickname: string;
+  name: string;
+  totalScore: number;
+  bestScore: number;
+  wins: number;
+  totalGames: number;
+}
+
+export interface Leaderboard {
+  gameId: string;
+  gameName: string;
+  entries: LeaderboardEntry[];
+  generatedAt: string;
+}
+
+export interface GameHistory {
+  id: string;
+  duration: string;
+  state: "win" | "lose" | "draw";
+  score: number;
+  totalDamage: number;
+  createdAt: string;
+  game: {
+    id: string;
+    name: string;
+    gameLogo: string;
+  };
+}
+
+export interface User {
+  id: string;
+  name: string;
+  nickname: string;
+  email: string;
+  avatar: string;
+  coins: number;
+  experience: number;
+  totalScore: number;
+  level: {
+    id: string;
+    number: number;
+    name: string;
+    symbol: string;
+    experienceRequired: number;
+  };
+  nextLevelExperience: number;
+  levelProgress: number;
+  experienceToNextLevel: number;
+}
