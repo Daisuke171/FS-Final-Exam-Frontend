63c3419c4a85b37d6fbcb1cc0efe5caf936916ac
Nicron <nicron007@gmail.com>
2025-10-18 02:41:22 +0000

    feat(user-types): created a new type file for the user data

---
+++ types/user.types.ts
@@ -0,0 +1,44 @@
+export interface SkinWithStatus {
+  id: string;
+  name: string;
+  img: string;
+  level: number;
+  isUnlocked: boolean;
+  isOwned: boolean;
+  isActive: boolean;
+}
+
+export interface Skin {
+  id: string;
+  name: string;
+  img: string;
+  level: number;
+}
+
+export interface UserSkin {
+  id: string;
+  active: boolean;
+  skinId: string;
+  skin: Skin;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  nickname: string;
+  email: string;
+  coins: number;
+  experience: number;
+  totalScore: number;
+  skins: UserSkin[];
+  level: {
+    id: string;
+    atomicNumber: number;
+    name: string;
+    chemicalSymbol: string;
+    experienceRequired: number;
+  };
+  nextLevelExperience: number;
+  levelProgress: number;
+  experienceToNextLevel: number;
+}
