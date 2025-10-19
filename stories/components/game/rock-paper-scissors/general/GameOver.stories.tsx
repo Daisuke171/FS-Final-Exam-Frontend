949dfe96d7fbe07488374b8f6d62099912c3152a
Nicron <nicron007@gmail.com>
2025-10-19 02:01:09 +0000

    feat(storybook): created new use cases for the game-over story

--- stories/components/game/rock-paper-scissors/general/GameOver.stories.tsx
+++ stories/components/game/rock-paper-scissors/general/GameOver.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 import GameOver from "@/components/game/rock-paper-scissors/general/GameOver";
+import { pl } from "zod/locales";
 
 const meta: Meta<typeof GameOver> = {
   title: "Game/RockPaperScissors/GameOver",
@@ -13,6 +14,9 @@ const meta: Meta<typeof GameOver> = {
       },
     },
   },
+  argTypes: {
+    showXp: { control: "boolean" },
+  },
 };
 
 export default meta;
@@ -24,6 +28,104 @@ export const Default: Story = {
     playerId: "player1",
     players: [{ id: "player1" }, { id: "player2" }],
     confirmedPlayers: ["player1", "player2"],
+    scores: { player1: 100, player2: 0 },
+    xpData: {
+      player1: {
+        xpGained: 40,
+        leveledUp: false,
+        oldLevel: 1,
+        newLevel: 2,
+        unlockedSkins: [],
+        xpInCurrentLevelBefore: 0,
+        xpNeededForLevelBefore: 100,
+        progressBefore: 0,
+        xpInCurrentLevelAfter: 20,
+        xpNeededForLevelAfter: 200,
+        progressAfter: 40,
+        oldLevelName: "Hydrogen",
+        oldLevelSymbol: "H",
+        oldLevelColor: "blue",
+        newLevelName: "Helium",
+        newLevelSymbol: "He",
+        newLevelColor: "yellow",
+      },
+    },
+    showXp: true,
+    action: () => console.log("Play again clicked"),
+  },
+};
+
+export const LeveledUp: Story = {
+  args: {
+    gameWinner: "player1",
+    playerId: "player1",
+    players: [{ id: "player1" }, { id: "player2" }],
+    confirmedPlayers: ["player1", "player2"],
+    scores: { player1: 100, player2: 0 },
+    xpData: {
+      player1: {
+        xpGained: 100,
+        leveledUp: true,
+        oldLevel: 1,
+        newLevel: 2,
+        unlockedSkins: [],
+        xpInCurrentLevelBefore: 0,
+        xpNeededForLevelBefore: 80,
+        progressBefore: 0,
+        xpInCurrentLevelAfter: 20,
+        xpNeededForLevelAfter: 200,
+        progressAfter: 10,
+        oldLevelName: "Hydrogen",
+        oldLevelSymbol: "H",
+        oldLevelColor: "blue",
+        newLevelName: "Helium",
+        newLevelSymbol: "He",
+        newLevelColor: "yellow",
+      },
+    },
+    showXp: true,
+    action: () => console.log("Play again clicked"),
+  },
+};
+
+export const LeveledUpWithSkins: Story = {
+  args: {
+    gameWinner: "player1",
+    playerId: "player1",
+    players: [{ id: "player1" }, { id: "player2" }],
+    confirmedPlayers: ["player1", "player2"],
+    scores: { player1: 100, player2: 0 },
+    xpData: {
+      player1: {
+        xpGained: 100,
+        leveledUp: true,
+        oldLevel: 1,
+        newLevel: 2,
+        unlockedSkins: [
+          { name: "Skin 1", id: "skin1", img: "/avatars/gon.webp", level: 2 },
+          {
+            name: "Skin 2",
+            id: "skin2",
+            img: "/avatars/killua.webp",
+            level: 2,
+          },
+        ],
+        xpInCurrentLevelBefore: 0,
+        xpNeededForLevelBefore: 80,
+        progressBefore: 0,
+        xpInCurrentLevelAfter: 20,
+        xpNeededForLevelAfter: 200,
+        progressAfter: 10,
+        oldLevelName: "Hydrogen",
+        oldLevelSymbol: "H",
+        oldLevelColor: "blue",
+        newLevelName: "Helium",
+        newLevelSymbol: "He",
+        newLevelColor: "yellow",
+      },
+    },
+    showXp: true,
+    setShowXp: (value: boolean) => console.log("setShowXp called with:", value),
     action: () => console.log("Play again clicked"),
   },
 };
