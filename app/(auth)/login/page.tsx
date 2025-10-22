72ee609e63c7925f474257a6fee81eaa9ca37d74
Nicron <nicron007@gmail.com>
2025-10-22 03:27:15 +0000

    feat(login): created a login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginCard from "./login-card";
+
+export default function LogIn() {
+  return (
+    <div className="mt-[calc(75px+2.5rem)] flex items-center justify-center">
+      <LoginCard />
+    </div>
+  );
+}
