2d68613827962d72d0aa4d66a94825d5c416aba1
Nicron <nicron007@gmail.com>
2025-10-22 03:17:19 +0000

    feat(apollo-client-server): created a server component of apollo client to use it in the auth.ts

---
+++ lib/apollo-client-server.tsx
@@ -0,0 +1,15 @@
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+
+export function createServerApolloClient(token?: string) {
+  return new ApolloClient({
+    ssrMode: true,
+    link: new HttpLink({
+      uri:
+        process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:3010/graphql",
+      headers: token ? { authorization: `Bearer ${token}` } : {},
+    }),
+    cache: new InMemoryCache(),
+  });
+}
+
+export const apolloClientServer = createServerApolloClient();
