de9708b6fce540095f67084d795e076f671091ca
Nicron <nicron007@gmail.com>
2025-10-22 02:26:27 +0000

    refactor(games): removed some navbar components and refactored some styles

--- app/games/coding-war/page.tsx
+++ app/games/coding-war/page.tsx
@@ -1,16 +1,8 @@
-import Navbar from "@/components/ui/general/landing-page/Navbar";
 import MainCard from "./main-card";
-
-const MOCK_NAV_PROPS = {
-  username: "Nombre", 
-  avatar: "/default-pfp.jpg", 
-  users: 124 
-};
 
 export default function Home() {
   return (
     <>
-      <Navbar {...MOCK_NAV_PROPS} />
       <div className="flex flex-col h-screen items-center justify-center bg-gradient-one">
         <MainCard />
       </div>
