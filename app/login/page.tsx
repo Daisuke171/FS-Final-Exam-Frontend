14a39f0e400652129628b0e04125a0ba524df5dc
Nicron <nicron007@gmail.com>
2025-10-24 09:03:10 +0000

    feat(middleware): protect the login, register and profile routes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { redirect } from "next/navigation";
 import LoginCard from "./login-card";
+import { useSession } from "next-auth/react";
 
 export default function LogIn() {
+  const { status } = useSession();
+  const isAuthenticated = status === "authenticated";
+
+  if (isAuthenticated) {
+    redirect("/profile");
+  }
   return (
-    <div className="mt-[calc(75px+2.5rem)] flex items-center justify-center">
+    <div className="flex pt-[calc(75px+2.5rem)] mb-[calc(57px+2.5rem)] md:mb-10 h-screen items-center justify-center">
       <LoginCard />
     </div>
   );
