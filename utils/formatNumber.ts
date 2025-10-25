3c9f558b18fd2c3bcdfe2f4fa0496a3a06cb04da
Nicron <nicron007@gmail.com>
2025-10-25 23:43:00 +0000

    refactor(format-numbers): modify the format numbers function

---
+++ utils/formatNumber.ts
@@ -0,0 +1,7 @@
+export function formatNumber(value: number) {
+  if (value >= 1_000_000)
+    return (value / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M";
+  if (value >= 1_000)
+    return (value / 1_000).toFixed(1).replace(/\.0$/, "") + "k";
+  return Math.round(value).toString();
+}
