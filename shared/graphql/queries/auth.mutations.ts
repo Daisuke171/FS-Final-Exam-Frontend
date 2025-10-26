d225a6515b576ae70432924de72a8eebaed20126
Nicron <nicron007@gmail.com>
2025-10-26 05:52:14 +0000

    feat(queries): add new mutation for the refresh token and for the logout

--- shared/graphql/queries/auth.mutations.ts
+++ shared/graphql/queries/auth.mutations.ts
@@ -4,6 +4,7 @@ export const LOGIN_MUTATION = gql`
   mutation Login($loginInput: LoginInput!) {
     login(loginInput: $loginInput) {
       accessToken
+      refreshToken
       user {
         id
         username
@@ -40,5 +41,33 @@ export const REGISTER_MUTATION = gql`
       lastname
       birthday
     }
+  }
+`;
+
+export const REFRESH_TOKEN_MUTATION = gql`
+  mutation RefreshAccessToken($refreshToken: String!) {
+    refreshAccessToken(refreshToken: $refreshToken) {
+      accessToken
+      refreshToken
+      user {
+        id
+        name
+        lastname
+        nickname
+        username
+        email
+        skins {
+          skin {
+            img
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const LOGOUT_MUTATION = gql`
+  mutation Logout {
+    logout
   }
 `;
