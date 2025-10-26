ed7ec88cb4d8f60079b7fc0f783e1bed7acff600
Agustin <deadmanwonderland171@gmail.com>
2025-10-25 22:24:51 -0300

    vercel: fixes 4

--- modules/chat/index.ts
+++ modules/chat/index.ts
@@ -1,5 +1,4 @@
 export * from "./model/types";
-export { useChat } from "./model/useChat";
 export { default as ChatWindow } from "./ui/ChatWindow";
 export { GlobalChatProvider } from "./provider/GlobalChatProvider";
 export { useUnreadStore } from "./model/unread.store";
