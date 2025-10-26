3030de4d8022a3faf2ec6ed066e9b6c29b621e94
Agustin <deadmanwonderland171@gmail.com>
2025-10-26 15:04:26 -0300

    chore: readded footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default async function RootLayout({
           <NavbarServer />
           {children}
           <BottomBar />
+          <Footer/>
         </Providers>
       </body>
     </html>
