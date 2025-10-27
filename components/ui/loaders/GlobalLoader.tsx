075538edd692be60670223bd850beb71a5cbc9d7
Nicron <nicron007@gmail.com>
2025-10-27 05:27:25 +0000

    feat(loader): create a general loader

---
+++ components/ui/loaders/GlobalLoader.tsx
@@ -0,0 +1,15 @@
+import { Icon } from "@iconify/react";
+
+export default function GlobalLoader() {
+  return (
+    <div className="w-full h-screen bg-background flex items-center justify-center">
+      <div className="flex flex-col gap-8 items-center">
+        <p className="text-2xl text-font font-medium">Cargando...</p>
+        <Icon
+          icon="svg-spinners:blocks-shuffle-3"
+          className="text-[120px] text-bright-purple"
+        />
+      </div>
+    </div>
+  );
+}
