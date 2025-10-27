8c7d868a665f519474e790eec98215aa86bd1ab6
Nicron <nicron007@gmail.com>
2025-10-27 05:22:49 +0000

    feat(profile): now handle loading state

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,17 +7,21 @@ import AchievementsComponent from "./achievements-component";
 import useBreakpoint from "@/hooks/useBreakpoint";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import GlobalLoader from "@/components/ui/loaders/GlobalLoader";
 
 export default function Profile() {
   const breakpoint = useBreakpoint();
   const { status } = useSession();
   const isAuthenticated = status === "authenticated";
+  const isLoading = status === "loading";
+
+  if (isLoading) return <GlobalLoader />;
   if (!isAuthenticated) {
     redirect("/login");
   }
 
   return (
-    <div className="flex flex-col gap-5 items-center mb-[calc(57px+2.5rem)] md:mb-10 mt-[calc(75px+2.5rem)]">
+    <div className="flex flex-col gap-5 items-center mb-10 mt-[calc(75px+2.5rem)]">
       <div className="flex flex-col w-full items-center">
         <div className="w-[95%] max-w-350 h-30 bg-dark-blue rounded-t-xl border-b-2 border-dark-gray"></div>
         <ProfileHeader />
