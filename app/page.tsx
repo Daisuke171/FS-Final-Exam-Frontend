35d069282d316ad961e4e49707adf346ede3e046
Nicron <nicron007@gmail.com>
2025-10-28 20:46:09 +0000

    fix conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-export default function Home() {
-  return (
-    <div
-      className="
-    "
-    ></div>
-  );
-=======
 import LandingPage from "@/components/ui/general/landing-page/LandingPage";
 
 export default function HomePage() {
   return <LandingPage />;
->>>>>>> ca63e3cdd084f1f60f9262099e15bb33d6011c13
 }
